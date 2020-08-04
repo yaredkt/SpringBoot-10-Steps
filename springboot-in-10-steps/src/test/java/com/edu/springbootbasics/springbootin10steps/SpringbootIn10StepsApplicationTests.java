c1bbcd29eb40f5c78ccba10d03c05a1485e6dc35
yaried tekie <yaredsonj@gmail.com>
2020-08-03 22:00:03 -0400

    springboot in 10 steps first project creation

---
+++ springboot-in-10-steps/src/test/java/com/edu/springbootbasics/springbootin10steps/SpringbootIn10StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.springbootbasics.springbootin10steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIn10StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
